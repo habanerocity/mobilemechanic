3556f08b68bd7f081cb47dadc5500abaa7ca9dd5
habanerocity <lindyo87@gmail.com>
2022-07-03 09:17:27 -0700

    changed title

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import styles from './_App.module.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-import React, { useState } from 'react';
-import Header from './Components/UI/Header';
-import Features from './Components/Features/Features';
-import ServicesSection from './Components/ServicesSection/ServicesSection';
-import MapSection from './Components/MapSection/MapSection';
-import SliderSection from './Components/SliderSection/SliderSection';
-import GetStartedSection from './Components/GetStartedSection/GetStartedSection';
-import Banner from './Components/Footer/Banner';
-import Footer from './Components/Footer/Footer';
+import styles from "./_App.module.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import Header from "./Components/UI/Header";
+import Features from "./Components/Features/Features";
+import ServicesSection from "./Components/ServicesSection/ServicesSection";
+import MapSection from "./Components/MapSection/MapSection";
+import SliderSection from "./Components/SliderSection/SliderSection";
+import GetStartedSection from "./Components/GetStartedSection/GetStartedSection";
+import Banner from "./Components/Footer/Banner";
+import Footer from "./Components/Footer/Footer";
 
 // import LoadingSpinner from './Components/UI/LoadingSpinner';
 
-import BtnContext from './store/btn-context';
+import BtnContext from "./store/btn-context";
 
 // const ServicesSection = React.lazy(() => import('./Components/ServicesSection/ServicesSection'));
 
 function App() {
-	const [ getQuote, setGetQuote ] = useState(false);
+  const [getQuote, setGetQuote] = useState(false);
 
-	const handleClose = () => {
-		setGetQuote(false);
-	};
+  const handleClose = () => {
+    setGetQuote(false);
+  };
 
-	return (
-		<div className={styles.app}>
-			<BtnContext.Provider value={getQuote}>
-				<Header onClose={handleClose} />
-				<Features onConfirm={setGetQuote} />
-				<SliderSection />
-				<GetStartedSection onConfirm={setGetQuote} />
-				{/* <Suspense fallback={LoadingSpinner}> */}
-				<ServicesSection />
-				{/* </Suspense> */}
-				<MapSection />
-				<Banner onConfirm={setGetQuote} />
-			</BtnContext.Provider>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className={styles.app}>
+      <BtnContext.Provider value={getQuote}>
+        <Header onClose={handleClose} />
+        <Features onConfirm={setGetQuote} />
+        <SliderSection />
+        <GetStartedSection onConfirm={setGetQuote} />
+        {/* <Suspense fallback={LoadingSpinner}> */}
+        <ServicesSection />
+        {/* </Suspense> */}
+        <MapSection />
+        <Banner onConfirm={setGetQuote} />
+      </BtnContext.Provider>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/UI/Hero.js
+++ src/Components/UI/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
 		<div className={styles.hero}>
 			<NavBar />
 			<div className={styles.hero__header}>
-				<h1 className={styles.header}>Mobile Master Technician That Comes to You</h1>
+				<h1 className={styles.header}>San Fernando Valley Mobile Mechanic</h1>
 				<p className={styles.subtext}>
 					Get auto solutions straight to your doorstep or office at a fair and affordable price
 				</p>
